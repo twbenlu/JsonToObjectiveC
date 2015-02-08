825cd4370a51bde228029ef0f82e6bd2d62f31df
benlu <benlu@benlutekiMacBook-Pro.local>
2015-02-08 21:19:52 +0800

    Initial Commit

---
+++ JsonToObjectiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  JsonToObjectiveC
+//
+//  Created by benlu on 2/8/15.
+//  Copyright (c) 2015 benlu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
